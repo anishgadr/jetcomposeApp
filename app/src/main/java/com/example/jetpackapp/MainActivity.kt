8403ec1e13e5a2903049fd6361faaaac3ce43a56
Anish G <anish@masalabox.com>
2022-07-25 22:11:14 +0530

    jetCompose

---
+++ app/src/main/java/com/example/jetpackapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.jetpackapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackapp.ui.theme.JetPackAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(modifier = Modifier
+                .background(Color.Blue)
+                .fillMaxSize(), verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Anish")
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(text = "G")
+            }
+        }
+    }
+}
+
